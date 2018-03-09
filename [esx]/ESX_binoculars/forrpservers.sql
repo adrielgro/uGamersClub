ea73ae18b60e0cfbb537777b32576af9005be4f3
Maquina <fivem@ugamers.com>
2018-03-09 18:37:04 +0000

    Subir carpetas

---
+++ [esx]/ESX_binoculars/forrpservers.sql
@@ -0,0 +1,4 @@
+INSERT INTO `shops` (`name`, `item`, `price`) VALUES
+('TwentyFourSeven', 'binoculars', 1000),
+('RobsLiquor', 'binoculars', 1000),
+('LTDgasoline', 'binoculars', 1000);
---
+++ [esx]/ESX_binoculars/sql.sql
@@ -0,0 +1,2 @@
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+('binoculars', 'Binoculars', 1, 0, 1);
