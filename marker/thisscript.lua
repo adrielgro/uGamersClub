fa574a6d8b3fd1efd2eff356ba57c261a1f91925
Maquina <fivem@ugamers.com>
2018-03-09 22:02:27 +0000

    Subir carpeta

---
+++ marker/thisscript.lua
@@ -0,0 +1,10 @@
+Citizen.CreateThread(function()
+
+local blipboat = AddBlipForCoord(-714.357421875, -1297.3057861328, 4.1019196510315)
+SetBlipSprite(blipboat, 410) 
+SetBlipDisplay(blipboat, 3)
+local blippet = AddBlipForCoord(562.19805908203, 2741.3090820313, 41.868915557861)
+SetBlipSprite(blippet, 141) 
+SetBlipDisplay(blippet, 3)
+end)
+
