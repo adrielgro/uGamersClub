1f90b1e3aa5ebb9cd413c9f0c9d3929e42c668a0
Adriel Guerrero <adrielgro@gmail.com>
2018-03-10 00:00:07 -0800

    Cambio de variables

--- [esx]/esx_voice/client/main.lua
+++ [esx]/esx_voice/client/main.lua
@@ -19,7 +19,7 @@ function drawLevel(r, g, b, a)
   SetTextOutline()
   SetTextEntry("STRING")
   AddTextComponentString("~y~Voz:~s~ " .. voice.level)
-  DrawText(0.175, 0.96)
+  DrawText(0.175, 0.95)
 end
 
 AddEventHandler('onClientMapStart', function()
