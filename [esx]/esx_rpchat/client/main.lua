9d83b23b94e08c75b624ab5efebd5f24297e57af
Adriel Guerrero <adrielgro@gmail.com>
2018-03-14 23:01:12 -0800

    modificacion de mensajes

--- [esx]/esx_rpchat/client/main.lua
+++ [esx]/esx_rpchat/client/main.lua
@@ -20,9 +20,9 @@ AddEventHandler('sendProximityMessageMe', function(id, name, message)
   local myId = PlayerId()
   local pid = GetPlayerFromServerId(id)
   if pid == myId then
-    TriggerEvent('chatMessage', "", {255, 0, 0}, " ^6 " .. name .." dice".."^6 " .. message)
+    TriggerEvent('chatMessage', "", {255, 0, 0}, " ^6* " .. name .." ".."^6 " .. message .. " *")
   elseif GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(myId)), GetEntityCoords(GetPlayerPed(pid)), true) < 19.999 then
-    TriggerEvent('chatMessage', "", {255, 0, 0}, " ^6 " .. name .." dice".."^6 " .. message)
+    TriggerEvent('chatMessage', "", {255, 0, 0}, " ^6 " .. name .." ".."^6 " .. message)
   end
 end)
 
@@ -31,8 +31,8 @@ AddEventHandler('sendProximityMessageDo', function(id, name, message)
   local myId = PlayerId()
   local pid = GetPlayerFromServerId(id)
   if pid == myId then
-    TriggerEvent('chatMessage', "", {255, 0, 0}, " ^0* " .. name .." dice".."^0  " .. message)
+    TriggerEvent('chatMessage', "", {255, 0, 0}, " ^0* " .. name .."  ".."^0  " .. message)
   elseif GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(myId)), GetEntityCoords(GetPlayerPed(pid)), true) < 19.999 then
-    TriggerEvent('chatMessage', "", {255, 0, 0}, " ^0* " .. name .." dice".."^0  " .. message)
+    TriggerEvent('chatMessage', "", {255, 0, 0}, " ^0* " .. name .."  ".."^0  " .. message)
   end
 end)
