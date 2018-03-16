99843ba19382f6279a3a3dec7eaf4ae5d5e562b4
matixto <matixto_tnf@hotmail.com>
2018-03-16 01:06:02 +0000

    Traducción FINALIZADA

--- [esx]/esx_weashops/locales/es.lua
+++ [esx]/esx_weashops/locales/es.lua
@@ -4,7 +4,7 @@ Locales ['es'] = {
   ['not_enough_black'] = 'No tienes ~r~suficiente~s~ dinero negro',
   ['not_enough'] = 'No tienes ~r~suficiente~s~ dinero',
   ['shop'] = 'Tienda',
-  ['shop_menu'] = 'Presione ~INPUT_CONTEXT~ para acceder a la tienda.',
+  ['shop_menu'] = 'Presiona ~INPUT_CONTEXT~ para acceder a la tienda.',
   ['map_blip'] = 'Arsenal',
 
 }
