f7bfbcacf5543df08e8fcea15fb0f4b750218651
matixto <matixto_tnf@hotmail.com>
2018-03-16 01:35:54 +0000

    TRADUCCION COMPLETA HECHA, FALTA TEST

--- [esx]/esx_basicneeds/locales/es.lua
+++ [esx]/esx_basicneeds/locales/es.lua
@@ -1,10 +1,10 @@
 Locales['es'] = {
 
-	['used_bread'] = 'you have used 1x bread',
-	['used_water'] = 'you have used 1x water',
-	['used_beer'] = 'you have used 1x beer',
-	['used_lockpick'] = 'you have used 1x lockpick',
-	['used_vodka'] = 'you have used 1x vodka',
-	['used_coffee'] = 'you have used 1x coffee',
+	['used_bread'] = 'Te comiste un pan, estaba rico de cojones',
+	['used_water'] = 'Te bebiste un botellín de agua',
+	['used_beer'] = 'Te tomaste una Paulaner de marca blanca',
+	['used_lockpick'] = 'Utilizaste una ganzúa',
+	['used_vodka'] = 'Has consumido un chupito de Vodka',
+	['used_coffee'] = 'Has consumido una taza de Café',
 
 }
