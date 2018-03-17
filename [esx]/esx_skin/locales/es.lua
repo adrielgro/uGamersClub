716da93945ec059febf527d13ee09f478100cbac
matixto <matixto_tnf@hotmail.com>
2018-03-17 00:55:45 +0000

    TRADUCCION COMPLETA, FALTA TTEST

--- [esx]/esx_skin/locales/es.lua
+++ [esx]/esx_skin/locales/es.lua
@@ -1,8 +1,8 @@
 Locales['es'] = {
 
   ['skin_menu'] = 'Personaje',
-  ['use_rotate_view'] = 'use ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ y ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ para rotar la vista.',
-  ['skin'] = 'cambiar skin',
-  ['saveskin'] = 'guardar el skin en un archivo',
+  ['use_rotate_view'] = 'Usa ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ y ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ para rotar la vista.',
+  ['skin'] = 'Cambiar skin',
+  ['saveskin'] = 'Guardar skin',
 
 }
