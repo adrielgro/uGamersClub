adc26d6d4eaf94ab46fa5f09520318a840982831
matixto <matixto_tnf@hotmail.com>
2018-03-17 01:01:04 +0000

    TRADUCCION COMPLETA TEST

--- [esx]/esx_joblisting/locales/es.lua
+++ [esx]/esx_joblisting/locales/es.lua
@@ -1,7 +1,7 @@
 Locales['es'] = {
 
 	['new_job'] = '¡Tienes un trabajo nuevo!',
-	['access_job_center'] = 'Pulsa ~INPUT_PICKUP~ para entrar a la ~b~Oficina de Empleo~s~.',
-	['job_center'] = 'Oficina de Empleo',
+	['access_job_center'] = 'Pulsa ~INPUT_PICKUP~ para entrar a la ~b~Oficina de empleo~s~.',
+	['job_center'] = 'Oficina de empleo',
 
 }
