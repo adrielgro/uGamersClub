b6e657279a9a89cbebb7f40466564d6b8666a434
benitoll <benitoll@users.noreply.github.com>
2018-03-17 02:19:09 +0000

    Revisado

--- [esx]/esx_clotheshop/locales/es.lua
+++ [esx]/esx_clotheshop/locales/es.lua
@@ -1,9 +1,9 @@
 Locales['es'] = {
 
 	['valid_this_purchase'] = '¿Validar esta compra?',
-	['yes'] = 'Si',
+	['yes'] = 'Sí',
 	['no'] = 'No',
-	['name_outfit'] = 'Nombre del traje ?',
+	['name_outfit'] = '¿Nombre del traje?',
 	['not_enough_money'] = 'No tienes suficiente dinero',
 	['press_menu'] = 'Presiona ~INPUT_CONTEXT~ para acceder al menú',
 	['clothes'] = 'Ropa',
