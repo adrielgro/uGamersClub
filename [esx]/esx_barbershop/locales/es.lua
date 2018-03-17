49c2af60e36e4f4e0f6809a43201999d779b8adc
matixto <matixto_tnf@hotmail.com>
2018-03-17 02:08:35 +0000

    TRADUCCION ESP COMPLETA

--- [esx]/esx_barbershop/locales/es.lua
+++ [esx]/esx_barbershop/locales/es.lua
@@ -1,10 +1,10 @@
 Locales['es'] = {
 
-    ['valid_purchase'] = '¿validar esta compra?',
-    ['yes'] = 'Si',
+    ['valid_purchase'] = '¿Quieres realmente pagar por ello?',
+    ['yes'] = 'Sí',
     ['no'] = 'No',
-    ['not_enough_money'] = 'No tienes suficente dinero',
-    ['press_access'] = 'Presiona ~INPUT_CONTEXT~ para acceder al menú',
+    ['not_enough_money'] = 'No tienes suficente dinero.',
+    ['press_access'] = 'Presiona ~INPUT_CONTEXT~ para acceder al menú.',
     ['barber_blip'] = 'Barbero',
     ['you_paid'] = 'Has pagado ',
 }
