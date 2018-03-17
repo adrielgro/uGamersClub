d17ca6659313e3ae0715c8d70144d20c111b904f
benitoll <benitoll@users.noreply.github.com>
2018-03-17 02:09:43 +0000

    Terminado de traducir y revisado

--- [esx]/esx_coffee/locales/es.lua
+++ [esx]/esx_coffee/locales/es.lua
@@ -1,7 +1,7 @@
 Locales['es'] = {
 
-	['shop'] = 'shop',
-	['shops'] = 'shops',
+	['shop'] = 'tienda',
+	['shops'] = 'tiendas',
 	['press_menu'] = 'presiona ~INPUT_CONTEXT~ para acceder a la tienda.',
 	['bought'] = 'compraste ~b~1x ',
 	['not_enough'] = 'no tienes ~r~suficiente~s~ dinero.'
