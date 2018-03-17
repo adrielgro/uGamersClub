de1127ea2cfc1c46b121309f24767bffa4dd07f8
matixto <matixto_tnf@hotmail.com>
2018-03-17 03:47:56 +0000

    Correcion altura

--- [esx]/esx_joblisting/config.lua
+++ [esx]/esx_joblisting/config.lua
@@ -1,10 +1,10 @@
 Config              = {}
 Config.DrawDistance = 100.0
-Config.ZoneSize     = {x = 3.0, y = 3.0, z = 2.0}
+Config.ZoneSize     = {x = 2.0, y = 2.0, z = 1.0}
 Config.MarkerColor  = {r = 100, g = 100, b = 204}
 Config.MarkerType   = 1
 Config.Locale = 'es'
 
 Config.Zones = {
-	{x = -389.9728, y = 6050.4394, z = 30.97847}
+	{x = -389.9728, y = 6050.4394, z = 30.2996}
 }
